da4f91044627a147df9ad46fead84f1fe30098d3
Alex Calvelage <alexcalvelage97@gmail.com>
2024-07-18 21:18:28 -0400

    V1.01
    -Fixed being able to enlarge multiple images at once
    -Added zm_cinema page
    -Fixed project-item descriptions trailing off the page
    -Added scrollbar to see more than 3 project-items

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const projectItems = document.querySelectorAll('[data-type="enlarge"]');
-  
-    projectItems.forEach(item => {
-      item.addEventListener('click', function() {
-        // Toggle 'enlarged' class on the clicked item
-        this.classList.toggle('enlarged');
+  const projectItems = document.querySelectorAll('[data-type="enlarge"]');
+  let enlargedItem = null;
+
+  projectItems.forEach(item => {
+      item.addEventListener('click', function(event) {
+          event.stopPropagation(); // Prevent the click event from propagating to the document
+          if (enlargedItem && enlargedItem !== this) {
+              enlargedItem.classList.remove('enlarged');
+          }
+          this.classList.toggle('enlarged');
+          enlargedItem = this.classList.contains('enlarged') ? this : null;
       });
-    });
   });
-  
+
+  document.addEventListener('click', function() {
+      if (enlargedItem) {
+          enlargedItem.classList.remove('enlarged');
+          enlargedItem = null;
+      }
+  });
+});
