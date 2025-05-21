2bb78df8ddb0b8d28b531097b728e0d2ec351f7e
Alex Calvelage <alexcalvelage97@gmail.com>
2025-05-20 23:43:46 -0400

    -Dialog system for screenshots
    -Reworked the viewable screenshots to use Dialog()
    ***TODO: Add support for arrowkeys to go through screenshots
    -Improved on how margins/padding/transforms are calculated to be more consistent across screens
    -Project containers will now scroll horizontally when out of space

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const projectItems = document.querySelectorAll('[data-type="enlarge"]');
-  let enlargedItem = null;
-
-  projectItems.forEach(item => {
-      item.addEventListener('click', function(event) {
-          event.stopPropagation(); // Prevent the click event from propagating to the document
-          if (enlargedItem && enlargedItem !== this) {
-              enlargedItem.classList.remove('enlarged');
-          }
-          this.classList.toggle('enlarged');
-          enlargedItem = this.classList.contains('enlarged') ? this : null;
-      });
-  });
-
-  document.addEventListener('click', function() {
-      if (enlargedItem) {
-          enlargedItem.classList.remove('enlarged');
-          enlargedItem = null;
-      }
-  });
-});
+const dialog = document.getElementById("dialog-screenshot");
+const selected_image = document.getElementById("selected-image");
+function openSCRViewer(button) {
+    const image = button.querySelector("img");
+    selected_image.src = image.src;
+    dialog.showModal();
+}
