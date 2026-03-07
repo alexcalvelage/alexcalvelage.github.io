72a3b6a1a4bdffc37349bc7bfdfa965f3bc28fc9
Alex Calvelage <alexcalvelage97@gmail.com>
2026-03-07 02:46:01 -0500

    +Picture dialog improvements
    +Screenshot dialogs can now be closed by clicking anywhere
    +Added zm_nightmare page
    +Updated footer

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-const dialog = document.getElementById("dialog-screenshot");
-const selected_image = document.getElementById("selected-image");
-function openSCRViewer(button) {
-    const image = button.querySelector("img");
-    selected_image.src = image.src;
-    dialog.showModal();
-}
+document.addEventListener("DOMContentLoaded", () => {
+
+    fetch("/footer.html")
+        .then(res => res.text())
+        .then(html => {
+            document.getElementById("footer").innerHTML = html;
+        });
+
+    const dialog = document.getElementById("dialog-screenshot");
+    const selected_image = document.getElementById("selected-image");
+
+    function openSCRViewer(button) {
+        const image = button.querySelector("img");
+        if (!image) return;
+        selected_image.src = image.src;
+        dialog.showModal();
+    }
+
+    dialog.addEventListener("click", () => dialog.close());
+
+    window.openSCRViewer = openSCRViewer;
+
+});
